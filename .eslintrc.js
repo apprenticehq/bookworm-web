7ed5353657afca238e9dc1c2db496b90829982a9
Yuki Yamada <yamadayuuki0818@gmail.com>
2016-09-26 20:26:57 +0900

    Add some eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,9 @@
 module.exports = {
   root: true,
   extends: 'react-app',
+  plugins: [
+    'import',
+  ],
   rules: {
     // Normal ESLint
     // Errors
@@ -50,6 +53,7 @@ module.exports = {
     'no-var': 'error',
     'one-var': ['error', 'never'],
     'operator-linebreak': ['error', 'after'],
+    'object-curly-spacing': ['error', 'always'],
     'padded-blocks': ['error', 'never'],
     'prefer-arrow-callback': 'off', // Wishlist, one day
     'prefer-const': 'error',
@@ -62,5 +66,51 @@ module.exports = {
     'space-unary-ops': ['error', { words: true, nonwords: false }],
     'spaced-comment': 'error',
     'yoda': 'error',
+
+    // Disabled
+    'strict': 'off',
+    'no-case-declarations': 'off',
+
+    // React
+    // Error
+    'react/display-name': 'error',
+    'react/jsx-boolean-value': ['error', 'always'],
+    'react/jsx-closing-bracket-location': 'error',
+    'react/jsx-curly-spacing': 'error',
+    'react/jsx-equals-spacing': 'error',
+    'react/jsx-first-prop-new-line': ['error', 'multiline'],
+    'react/jsx-handler-names': 'error',
+    'react/jsx-indent-props': ['error', 2],
+    'react/jsx-max-props-per-line': ['error', {maximum: 3}],
+    'react/jsx-no-duplicate-props': 'error',
+    'react/jsx-no-undef': 'error',
+    'react/jsx-pascal-case': 'error',
+    'react/jsx-space-before-closing': 'error',
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'react/jsx-wrap-multilines': 'error',
+    'react/no-danger': 'error',
+    'react/no-did-mount-set-state': 'error',
+    'react/no-did-update-set-state': 'error',
+    'react/no-direct-mutation-state': 'error',
+    'react/no-multi-comp': 'off', // Wishlist, one day
+    'react/no-unknown-property': 'error',
+    'react/no-is-mounted': 'error',
+    'react/prefer-arrow-callback': 'off', // Wishlist, one day
+    'react/prefer-es6-class': 'error',
+    'react/prop-types': 'error',
+    'react/react-in-jsx-scope': 'error',
+    'react/self-closing-comp': 'error',
+    'react/sort-comp': 'error',
+    'react/sort-prop-types': 'error',
+    'react/jsx-indent': ['error', 2],
+
+    // Disabled
+    'react/jsx-no-literals': 'off',
+    'react/jsx-sort-props': 'off',
+    'react/no-set-state': 'off',
+
+    // Import
+    'import/extensions': 'error',
   },
 };
